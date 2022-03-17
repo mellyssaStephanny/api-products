f86ea05fe60d91dbf69d8581d77065c8c3d2cfa9
mellyssaStephanny <mellyssah.mendes@live.com>
2022-03-17 14:12:39 -0300

    create router

---
+++ myapp/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include 
+from catalog.views import ProductsViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'products', ProductsViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+]
