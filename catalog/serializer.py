e368e15f35e2faafe38f2c9848cc48c11b5a06eb
mellyssaStephanny <mellyssah.mendes@live.com>
2022-03-18 02:43:24 -0300

    add serializer

---
+++ catalog/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from catalog.models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'sku', 'name', 'qtd',
+                  'price', 'barcode', 'description']
