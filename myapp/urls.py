6a51de63c960e2593869077d738f1993cdbf9015
mellyssaStephanny <mellyssah.mendes@live.com>
2022-03-18 03:15:08 -0300

    routes

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
 from catalog.views import ProductsViewSet
 from rest_framework import routers
 
