9f7bd3ef2a38cb29c5ec54ec451479552f297f35
mellyssaStephanny <mellyssah.mendes@live.com>
2022-03-18 03:16:18 -0300

    refactor views

--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,6 @@ from catalog.models import Product
 from catalog.serializer import ProductSerializer
 
 
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductsViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
