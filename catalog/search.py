0e36c9433aba47182cc4be9258d7ad4f7ec38b3f
mellyssaStephanny <mellyssah.mendes@live.com>
2022-03-18 14:45:41 -0300

    add result in elastic

--- catalog/search.py
+++ catalog/search.py
@@ -3,4 +3,18 @@ from elasticsearch_dsl import Search
 
 client = Elasticsearch()
 
-s = Search(using=client)
+product = Product(
+    sku="SG-S22-5G-BL",
+    name="Galaxy S22 Ultra 5G",
+    qtd=10,
+    price="7884.90",
+    barcode="7892509122580",
+    type=2,
+    description="Smartphone Samsung Galaxy S22 Ultra 12GB RAM Rosa"
+)
+product.save()
+
+s = ProductDocument.search().query("match", description="Rosa")
+
+for hit in s:
+    print("Product name : {}, description {}".format(hit.name, hit.description))
