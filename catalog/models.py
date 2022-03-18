51b15cf5aa2b773348f1fcadbc0976986a56b767
mellyssaStephanny <mellyssah.mendes@live.com>
2022-03-18 02:24:01 -0300

    create model

---
+++ catalog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Product(models.Model):
+    sku = models.CharField(max_length=13)
+    qtd = models.IntegerField()
+    name = models.CharField(max_length=60)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    barcode = models.CharField(max_length=13) 
+    description = models.TextField()
+
+    def __str__(self):
+        return self.sku
