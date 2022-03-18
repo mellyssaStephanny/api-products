d93aa4e1b7c62c2e6eaff39a09f6cf4281efa328
mellyssaStephanny <mellyssah.mendes@live.com>
2022-03-18 14:16:47 -0300

    remove cache

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,8 @@
-from rest_framework.response import Response
 from rest_framework import viewsets
-from rest_framework import serializers
 from catalog.models import Product
-from django.views.decorators.cache import cache_page
+from catalog.serializer import ProductSerializer
 
 
 class ProductsViewSet(viewsets.ModelViewSet):
-
-    @cache_page(60*15)
-    def list_all(request):
-        queryset = Product.objects.all()
-        return Response(request)
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
